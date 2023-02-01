2ae9c9d8b9e8e587c2ffb37d89fb68ab4ab4327e
flaminshinjan <srikpatra.2008@gmail.com>
2023-02-01 23:45:03 +0530

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Shinjan Patra on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
